36e05d4e656ea569481df8cd87161cfe0ca58728
sampletext32 <sampletext32@bk.ru>
2020-04-25 07:39:09 +0300

    Add project

---
+++ Nikitina_3/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void main(void)
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Автор: Никитина Дарья Сергеевна\n";
+	cout << "Программа для расчёта срока обучения\n";
+
+	int k = 1;
+	int t = 0;
+
+	while(k <= 20)
+	{
+		t += k;
+		cout << "День " << k << ", смастерил " << k << ", за всё время " << t << "\n";
+		k++;
+	}
+	
+
+	system("PAUSE");
+}
